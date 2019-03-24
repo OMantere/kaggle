25b811cfb9e325d97fa2899f88b7db51e10b6598
Oskari Mantere <osku.mantere@gmail.com>
2019-03-24 13:18:18 +0200

    tooling up

---
+++ toolz.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import json
+import seaborn as sns
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import r2_score, mean_squared_error
+
+
+def checkna(X):
+    n = len(X)
+    nans = X.isna()
+    nan_sums = nans.sum()
+    nonzero_sums = nan_sums[nan_sums > 0]
+    if nonzero_sums.empty:
+        print("X contains no NaNs!")
+    else:
+        num_nans = len([i for _, i in nans.iterrows() if np.any(i)])
+        percentage = 100.0*num_nans/n
+        print("%d/%d (%d%%) rows contain NaNs, in %d columns:" % (num_nans, n, percentage, len(nonzero_sums)))
+        print(nan_sums.to_string())
+
+
+def plot_pairs(X):
+    sns.pairplot(X.dropna())
+
+
+def train_model(model, X, y):
+    y_pred = cross_val_predict(model, X_train, y_train, cv=5)
+    print("R2: {}".format(r2_score(y_pred, y_train)))
+    print("RMSLE: {}".format(np.sqrt(mean_squared_error(y_pred, y_train))))
+    plt.figure()
+    plt.scatter(y_pred, y_train)
+    plt.title("CV Results")
